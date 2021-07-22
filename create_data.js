dbcdcb4b9a33f1be92e9f2993c379b257c98b6b4
Bukhori Inst <58775021+bukhori-inst@users.noreply.github.com>
2021-07-22 12:19:23 +0700

    create data

---
+++ create_data.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/db-mongoose', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review: String,
+});
+
+const fruit = mongoose.model('fruit', fruitSchema);
+
+// const apple = new fruit({
+//   name: 'Apple',
+//   rating: 8,
+//   review: 'Rasanya manis',
+// });
+
+// apple.save(function (error) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log('Berhasil simpan buah apple ke dalam database');
+//   }
+// });
+
+const kiwi = new fruit({
+  name: 'Kiwi',
+  rating: 10,
+  review: 'Buah yang terbaik',
+});
+
+const jeruk = new fruit({
+  name: 'Jeruk',
+  rating: 5,
+  review: 'Asem rasanya',
+});
+
+const pisang = new fruit({
+  name: 'Pisang',
+  rating: 6,
+  review: 'Manis dan menyegarkan',
+});
+
+fruit.insertMany([kiwi, jeruk, pisang], function (error) {
+  if (error) {
+    console.log(error);
+  } else {
+    mongoose.connection.close();
+    console.log('Berhasil simpan buah kiwi, jeruk, pisang ke dalam database');
+  }
+});
